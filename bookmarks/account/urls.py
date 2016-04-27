dbc0c4333b074fa4b8a015c08a42ef3108ddcbe0
Rajat Vijay <rajatvijay5@gmail.com>
2016-04-27 18:59:03 +0530

    Added urls for password change form and password change successful meassage

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
   url(r'^login/$', 'django.contrib.auth.views.login', name='login'),
   url(r'^logout/$', 'django.contrib.auth.views.logout', name='logout'),
   url(r'^logout-then-login/$', 'django.contrib.auth.views.logout_then_login', name='logout_then_login'),
+  url(r'^password-change/$', 'django.contrib.auth.views.password_change', name='password_change'),
+  url(r'^password-change-done/$', 'django.contrib.auth.views.password_change_done', name='password_change_done')
 ]
