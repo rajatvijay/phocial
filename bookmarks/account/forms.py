17d7ab8b01ecc1732bee1708cb443c98e9bb8258
Rajat Vijay <rajatvijay5@gmail.com>
2016-04-28 03:53:25 +0530

    Added user registartion form

--- bookmarks/account/forms.py
+++ bookmarks/account/forms.py
@@ -1,5 +1,23 @@
-#from django import forms
+from django import forms
+from django.auth.contrib import User
 
 #class LoginForm(forms.Form):
 #  username = forms.CharField()
 #  password = forms.CharField(widget=forms.PasswordInput)
+
+class UserRegistrationForm(forms.ModelForm):
+  password = forms.CharField(label='Password', widget=forms.PasswordInput)
+  password2 = forms.CharField(label='Repeat Password', widget=forms.PasswordInput)
+  
+  class Meta:
+    model = User
+    fields = ('username', 'first_name', 'email')
+  
+  # You can provide a clean_<fieldname>() method to any of your form fields in
+  # order to clean the value or raise form validation errors for the specific field 
+  def clean_password2(self):
+    cd = self.cleaned_data
+    if cd['password'] != cd['password2']:
+      raise forms.ValidationError('Passwords don\'t match')
+      
+    return cd['password2']
