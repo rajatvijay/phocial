1556a1a79544594be3e11cfce9773cafcba80c4c
Rajat Vijay <rajatvijay5@gmail.com>
2016-04-28 18:31:05 +0530

    Overriden save method to download image from url and save it to the db

--- bookmarks/images/forms.py
+++ bookmarks/images/forms.py
@@ -1,5 +1,8 @@
 from django import forms
 from .models import Image
+from urllib import request
+from django.core.files.base import ContentFile
+from django.utils.text import slugify
 
 class ImageCreateForm(forms.ModelForm):
   class Meta:
@@ -19,3 +22,22 @@ class ImageCreateForm(forms.ModelForm):
       raise forms.ValidationError('The given URL does not match valid image extensions.')
     
     return url
+    
+  # We download image using the url provided bby the user
+  def save(self, force_insert=False, force_update=False, commit=True):
+    
+    # crate an instance of the Image model but dont save it to the db
+    image_instance = super(ImageCreateForm, self).save(commit=False)
+    
+    image_url = self.cleaned_data['url']
+    image_name = '{}.{}'.format(slugify(self.cleaned_data['title']), image_url.rsplit('.', 1)[1])
+    
+    # download the image form the given url
+    response = request.urlopen(image_url)
+    
+    # save the image with its name in the image field of the saved instance
+    # but dont save the instance to the db
+    image_instance.image.save(image_name, ContentFile(response.read()), save=False)
+    
+    if commit:
+      image_instance.save()
