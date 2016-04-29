15b53633e033b8ca50d4d2adc238a807b4e0e837
Rajat Vijay <rajatvijay5@gmail.com>
2016-04-29 18:47:07 +0530

    Multiplied random number generator by 99...

--- bookmarks/images/templates/bookmarklet_launcher.js
+++ bookmarks/images/templates/bookmarklet_launcher.js
@@ -4,7 +4,8 @@
   }
   else {
     var script= document.createElement('script');
-    script.setAttribute('src', 'http://127.0.0.1:8000/static/js/bookmarklet.js?r=' + Math.floor(Math.random()));
+    script.setAttribute('src', 'http://127.0.0.1:8000/static/js/bookmarklet.js?r=' 
+      + Math.floor(Math.random()*999999999999999999999999999));
     document.body.appendChild(script);
   }
 })();
