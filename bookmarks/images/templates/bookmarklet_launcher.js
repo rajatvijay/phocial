a469d16c3c96a550faebe6d6d0b33b79a3ae47df
Rajat Vijay <rajatvijay5@gmail.com>
2016-04-29 18:41:33 +0530

    Finally got the bookmarklet functionality working and learn a bit about debugging js using chrome console

--- bookmarks/images/templates/bookmarklet_launcher.js
+++ bookmarks/images/templates/bookmarklet_launcher.js
@@ -3,7 +3,8 @@
     myBookmarklet();
   }
   else {
-    document.body.appendChild(document.createElement(script).src='http://127.0.0.1:8000/static/js/bookmarklet.js?r='
-    + Math.floor(Math.random()*99999999999999999));
+    var script= document.createElement('script');
+    script.setAttribute('src', 'http://127.0.0.1:8000/static/js/bookmarklet.js?r=' + Math.floor(Math.random()));
+    document.body.appendChild(script);
   }
 })();
