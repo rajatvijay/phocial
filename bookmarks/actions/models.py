0b67ce82da4798c88b930b476970515ef9854346
rajatvijay <rajatvijay5@gmail.com>
2016-05-26 05:09:56 -0700

    Added GenericForeignKey and ContentType Foreign key to point to the User model

---
+++ bookmarks/actions/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+
+class Action(models.Model):
+    user = models.ForeignKey(User, related_name='action', db_index=True)
+    verb = models.CharField(max_length=250)
+    created = models.DateTimeField(auto_now_add=True, db_index=True)
+    target_ct = models.ForeignKey(ContentType, blank=True, null=True, related_name='target_obj')
+    target_id = models.PositiveIntegerField(null=True, blank=True, db_index=True)
+    target = GenericForeignKey('target_ct', 'target_id')
+
+    class Meta:
+        ordering = ('-created', )
+
