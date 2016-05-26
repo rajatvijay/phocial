b792e890703242cd0f00b2e7aff65f3e96435243
rajatvijay <rajatvijay5@gmail.com>
2016-05-26 05:58:12 -0700

    Avoided duplicate actions in activity stream

--- bookmarks/actions/utils.py
+++ bookmarks/actions/utils.py
@@ -1,6 +1,19 @@
 from django.contrib.contenttypes.models import ContentType
+from django.utils import timezone
 from .models import Action
 
 def create_action(user, verb, target=None):
-    action = Action(user=user, verb=verb, target=target)
-    action.save()
+
+    last_minute = timezone.now() - timezone.timedelta(seconds=60)
+    similar_actions = Action.object.filter(user_id=user.id, verb=verb, timestamp__gte=last_minute)
+
+    if target:
+        target_ct = ContentType.objects.get_for_model(target)
+        similar_actions = similar_actions.objects.filter(target_ct=target_ct, target_id=target.id)
+
+    if not similar_actions:
+        action = Action(user=user, verb=verb, target=target)
+        action.save()
+        return True
+
+    return False
