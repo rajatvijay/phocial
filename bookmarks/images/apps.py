847d406c73cfbfcb1151eedc58847be9a5096430
rajatvijay <rajatvijay5@gmail.com>
2016-05-30 00:09:18 -0700

    Registered signal receiver function

---
+++ bookmarks/images/apps.py
@@ -0,0 +1,9 @@
+from django.apps import AppConfig
+
+class ImagesConfig(AppConfig):
+    name = 'images'
+    verbose_name = 'Image Bookmarks'
+
+    def ready(self):
+        # import signal handlers
+        import images.signals
